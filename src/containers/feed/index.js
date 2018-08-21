4f8b76c94386d1936246e15870f7bb28c70bd63a
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-08-21 11:18:06 +0500

    Add font-awesome and feed UI

--- src/containers/feed/index.js
+++ src/containers/feed/index.js
@@ -8,7 +8,29 @@ class FeedContainer extends React.Component {
     return (
       <div className="page-wrap">
         <TopNav/>
-        <div className="container">
+
+        <div className="container mt-4">
+          <div className="header-row clearfix">
+            <div className="group-heading">
+              <h4>Today <span className="small text-muted">July 12, 2019</span></h4>
+            </div>
+            <div className="group-filters">
+              <a href="#" className="btn btn-light language-filter">
+                <i className="fa fa-filter mr-2"></i>
+                All Languages
+              </a>
+              <div className="view-type btn">
+                <a href="#" className="active">
+                  <i className="fa fa-list mr-2"></i>
+                  List
+                </a>
+                <a href="#">
+                  <i className="fa fa-table mr-2"></i>
+                  Grid
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css'
 
 import './global.css';
 import App from './app';
