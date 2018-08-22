352b2d17c5aa12ab435fa172c583d9b01a6d1140
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-08-22 15:05:55 +0500

    View filters

--- src/components/filters/view-filter/index.js
+++ src/components/filters/view-filter/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './styles.css';
 
-const ViewFilter = (props) => (
-  <div className="view-type-wrap">
-    <div className="view-type btn shadowed cursor-default">
-      <a href="#" className="active">
-        <i className="fa fa-list mr-2"></i>
-        List
-      </a>
-      <a href="#">
-        <i className="fa fa-table mr-2"></i>
-        Grid
-      </a>
-    </div>
-  </div>
-);
+class ViewFilter extends React.Component {
+  state = {
+    selected: 'grid'
+  };
+
+  changeSelected = (viewType) => {
+    this.setState({
+      selected: viewType
+    });
+  };
+
+  render() {
+    return (
+      <div className="view-type-wrap">
+        <div className="view-type btn shadowed cursor-default">
+          <a onClick={ () => this.changeSelected('grid') } href="javascript:void(0)" className={ classNames({ active: this.state.selected === 'grid' }) }>
+            <i className="fa fa-table mr-2"></i>
+            Grid
+          </a>
+          <a onClick={ () => this.changeSelected('list') } href="javascript:void(0)" className={ classNames({ active: this.state.selected === 'list' }) }>
+            <i className="fa fa-list mr-2"></i>
+            List
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ViewFilter;
