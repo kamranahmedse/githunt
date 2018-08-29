667f6920a2266142c62f21a5b827f3f39cfd562f
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-08-29 23:25:51 +0500

    Update npm scripts and launcher icon

--- src/components/launcher/index.js
+++ src/components/launcher/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './styles.css';
+import Logo from '../icons/logo';
 
 const Launcher = () => (
   <div className="launcher-body">
     <div className="logo-container">
-      <img src='/img/logo.svg' alt='GitHunt'/>
+      <Logo/>
       <div className="logo-text">
         <h4>Loading ..</h4>
         <p className="text-muted">We're testing your patience</p>
