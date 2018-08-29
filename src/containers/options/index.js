12d53e8e6688fd9ffcf8d19c5282ed84e4f8f60f
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-08-29 11:42:16 +0500

    Componentize options form

--- src/containers/options/index.js
+++ src/containers/options/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './styles.css';
 import Logo from '../../components/icons/logo';
 import { Link } from 'react-router-dom';
+import OptionsForm from '../../components/options-form';
 
 class OptionsContainer extends React.Component {
   render() {
@@ -20,24 +21,7 @@ class OptionsContainer extends React.Component {
         </div>
 
         <div className="container shadow">
-          <div className="form-field">
-            <h2>Add the Token</h2>
-            <p className="text-muted">Generate a token and add it below to avoid hitting the rate limit.</p>
-            <hr/>
-            <ul>
-              <li>
-                Go to the <a href="https://github.com/settings/tokens" target="_blank" rel="noopener noreferrer"><i className="fa fa-external-link mr-1"></i> Settings <i className="fa fa-angle-right"></i> Personal Access Tokens</a> of your github profile
-              </li>
-              <li>Click <span>Generate New Token</span>. Enter the description and select the <span>scope</span> called <span>public_repo</span> under repo and click <span>Generate Token</span>.</li>
-              <li>You will be presented with the generated token. Copy the token and add it below</li>
-            </ul>
-            <input type="text" className="form-control"/>
-          </div>
-          <button className="btn btn-dark btn-lg btn-save shadow">
-            <i className="fa fa-cog mr-2"></i>
-            Save Settings
-          </button>
-          <Link className='btn btn-primary shadow btn-block btn-lg' to='/'><i className="fa fa-arrow-left"></i> Go Home</Link>
+          <OptionsForm/>
         </div>
       </div>
     );
