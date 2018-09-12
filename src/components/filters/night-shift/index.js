d1dc466f03ba0d53a6c7ace95b9918523ec0e13f
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2018-09-12 14:59:11 +0430

    add night shift component with style

---
+++ src/components/filters/night-shift/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import BlubIcon from '../../icons/blub';
+import "./style.css";
+
+class NightShift extends React.Component {
+  render() {
+    return (
+      <div className="night__shift__box">
+        <BlubIcon />
+      </div>
+    )
+  }
+}
+
+
+export default NightShift;
