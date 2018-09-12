3eca26caf68dafdb07a5be8f2beb1a7d92cef15d
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2018-09-12 15:43:25 +0430

    add night shift reducer and actions

---
+++ src/components/filters/night-shift/actions.js
@@ -0,0 +1,11 @@
+
+export const NIGHT_SHIFT_ON = 'NIGHT_SHIFT_ON';
+export const NIGHT_SHIFT_OFF = 'NIGHT_SHIFT_OFF';
+
+export const on = () => ({
+  type: NIGHT_SHIFT_ON,
+});
+
+export const off = () => ({
+  type: NIGHT_SHIFT_OFF,
+});
--- src/components/filters/night-shift/index.js
+++ src/components/filters/night-shift/index.js
@@ -1,16 +1,37 @@
 import React, { Fragment } from "react";
+import { connect } from 'react-redux';
+import { on, off }from './actions';
 import BlubIcon from '../../icons/blub';
 import "./style.css";
 
 class NightShift extends React.Component {
+  constructor() {
+    super();
+    this.toggleNightShift = this.toggleNightShift.bind(this);
+  }
+  toggleNightShift() {
+    if (!this.props.nightShift) {
+      return this.props.on();
+    }
+    return this.props.off();
+  }
   render() {
     return (
-      <div className="night__shift__box">
+      <div className="night__shift__box" onClick={this.toggleNightShift}>
         <BlubIcon />
       </div>
     )
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    nightShift: state.nightShift
+  }
+}
+const mapDispatchToProps = {
+  on,
+  off
+};
 
-export default NightShift;
+export default connect(mapStateToProps, mapDispatchToProps)(NightShift);
