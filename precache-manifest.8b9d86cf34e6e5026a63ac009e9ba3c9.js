9a6c5a2a383df40c9676d298d93bc42dfeaf2445
Kamran Ahmed <kamranahmed.se@gmail.com>
2019-06-15 00:16:46 +0400

    Updates

---
+++ precache-manifest.8b9d86cf34e6e5026a63ac009e9ba3c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c4d3f149df00e799918a22eb607dd7",
+    "url": "/githunt/index.html"
+  },
+  {
+    "revision": "9c428fbb167c8fca44e2",
+    "url": "/githunt/static/css/2.c66f9ff3.chunk.css"
+  },
+  {
+    "revision": "2a4b09cc324430542c12",
+    "url": "/githunt/static/css/main.74b19a4b.chunk.css"
+  },
+  {
+    "revision": "9c428fbb167c8fca44e2",
+    "url": "/githunt/static/js/2.aeb34f43.chunk.js"
+  },
+  {
+    "revision": "2a4b09cc324430542c12",
+    "url": "/githunt/static/js/main.b7d243a0.chunk.js"
+  },
+  {
+    "revision": "2279193bd7ab0ba50519",
+    "url": "/githunt/static/js/runtime~main.09c9985d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/githunt/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/githunt/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/githunt/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/githunt/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/githunt/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/githunt/precache-manifest.30d6777e5f256779ac90a2441095850e.js"
+  "/githunt/precache-manifest.8b9d86cf34e6e5026a63ac009e9ba3c9.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/githunt/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/githunt/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
