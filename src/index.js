5df886226b46dd45a9716b33504fab8da0eafe06
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-05-16 14:57:05 +0400

    Add deps and chakra setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSReset, ThemeProvider } from "@chakra-ui/core";
+import { App } from "./app";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <ThemeProvider>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
