40e1c6a3dc7cb11506fa7c33f72dc208be042a3a
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-05-30 02:45:50 +0400

    Add view switcher

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Menu, MenuButton, MenuItem, MenuList, Select, Stack } from '@chakra-ui/core';
 import { FaList, FaTable } from 'react-icons/fa';
 
-import languages from '../data/languages';
+import languages from "../data/languages";
 
-export function Filters() {
+export function Filters(props) {
+  const { onViewChange } = props;
+  const [viewType, setViewType] = useState();
+  
+  useEffect(() => {
+    onViewChange(viewType);
+  }, [viewType]);
+
   return (
     <Stack isInline>
       <Select>
         {languages.map((language) => (
-          <option value={language.value}>{language.label}</option>
+          <option key={language.value} value={language.value}>
+            {language.label}
+          </option>
         ))}
       </Select>
 
       <Menu>
-        <MenuButton as={Button} bg='white' borderWidth={1} px='40px' fontWeight={400} leftIcon='calendar'>
+        <MenuButton
+          as={Button}
+          bg="white"
+          borderWidth={1}
+          px="40px"
+          fontWeight={400}
+          leftIcon="calendar"
+          _focus={{ boxShadow: 'none'}}
+        >
           Actions
         </MenuButton>
         <MenuList>
@@ -27,10 +44,38 @@ export function Filters() {
           </MenuItem>
         </MenuList>
       </Menu>
-      
-      <Stack isInline spacing={0} borderWidth={1} bg='white' rounded='5px' alignItems='center' ml='10px'>
-        <Button h='100%' fontWeight={400} roundedRight={0} leftIcon={FaTable} bg='white'>Grid</Button>
-        <Button h='100%' fontWeight={400} roundedLeft={0} leftIcon={FaList} bg='white'>List</Button>
+
+      <Stack
+        isInline
+        spacing={0}
+        borderWidth={1}
+        bg="white"
+        rounded="5px"
+        alignItems="center"
+        ml="10px"
+      >
+        <Button
+          h="100%"
+          onClick={() => setViewType("grid")}
+          fontWeight={400}
+          roundedRight={0}
+          leftIcon={FaTable}
+          bg={viewType === "grid" ? "gray.200" : "white"}
+          _focus={{ boxShadow: 'none'}}
+        >
+          Grid
+        </Button>
+        <Button
+          h="100%"
+          onClick={() => setViewType("list")}
+          fontWeight={400}
+          roundedLeft={0}
+          leftIcon={FaList}
+          bg={viewType === "list" ? "gray.200" : "white"}
+          _focus={{ boxShadow: 'none'}}
+        >
+          List
+        </Button>
       </Stack>
     </Stack>
   );
--- src/feed.js
+++ src/feed.js
@@ -1,42 +1,48 @@
-import React from 'react';
-import { Box, Flex, SimpleGrid, Button } from '@chakra-ui/core';
-import { PageHeader } from './components/page-header';
-import { GroupTitle } from './components/group-title';
-import { Filters } from './components/filters';
-import { Repo } from './components/repo';
+import React, { useState } from 'react';
+import { Box, Flex, SimpleGrid, Button } from "@chakra-ui/core";
+import { PageHeader } from "./components/page-header";
+import { GroupTitle } from "./components/group-title";
+import { Filters } from "./components/filters";
+import { Repo } from "./components/repo";
 
 export function Feed() {
-  return <Box maxWidth="1200px" mx="auto">
-    <PageHeader />
-    
-    <Flex alignItems='center' justifyContent='space-between' mb='25px'>
-      <GroupTitle />
-      <Filters />
-    </Flex>
-    
-    <SimpleGrid columns={3} spacing='15px'>
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-      <Repo />
-    </SimpleGrid>
-  
-    <Flex alignItems="center" justifyContent="center" my="20px">
-      <Button variantColor="blue">Load next group</Button>
-    </Flex>
-  </Box>;
+  const [viewType, setViewType] = useState();
+
+  return (
+    <Box maxWidth="1200px" mx="auto">
+      <PageHeader />
+
+      <Flex alignItems="center" justifyContent="space-between" mb="25px">
+        <GroupTitle />
+        <Filters onViewChange={(viewType) => {
+          setViewType(viewType);
+        }} />
+      </Flex>
+
+      <SimpleGrid columns={viewType === 'list' ? 1 : 3} spacing="15px">
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+        <Repo isListView={viewType === 'list'} />
+      </SimpleGrid>
+
+      <Flex alignItems="center" justifyContent="center" my="20px">
+        <Button variantColor="blue">Load next group</Button>
+      </Flex>
+    </Box>
+  );
 }
